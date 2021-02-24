7fc158685302b2fc4789fc5e60a64356f275c19e
linn-holmqvist-nti-johanneberg <74715034+linn-holmqvist-nti-johanneberg@users.noreply.github.com>
2021-02-24 21:47:10 +0100

    24-02-2021

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Variabel: let anka="En fin fågel";
+/* FUnktion: function namn(ev parametrar){
+    Kod här
+} */
+//Kör funktion: namn();
+
+
+//HTMLKNAPP: <button onclick=”function()”>Knapptext</button>
+// hämta knapp
+//let myButton = document.querySelector(”button”)
+
+// Lägg till ett “öra” som lyssnar på click
+//myButton.addEventlistener(”click”,show) 
+// Funktionen “show” körs nu vid klick.
+
