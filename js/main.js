ec57e97a39dcfb7276a439418539f497d7625f04
linn-holmqvist-nti-johanneberg <74715034+linn-holmqvist-nti-johanneberg@users.noreply.github.com>
2021-03-10 00:03:03 +0100

    03-09

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-// Variabel: let anka="En fin fågel";
-/* FUnktion: function namn(ev parametrar){
-    Kod här
-} */
-//Kör funktion: namn();
+function showmenu(){
+     let menu = document.querySelector("div.menu_mobil");
+     menu.classList.toggle("show");
 
+     let close = document.querySelector(".clear");
+     close.classList.toggle("visible");
 
-//HTMLKNAPP: <button onclick=”function()”>Knapptext</button>
-// hämta knapp
-//let myButton = document.querySelector(”button”)
-
-// Lägg till ett “öra” som lyssnar på click
-//myButton.addEventlistener(”click”,show) 
-// Funktionen “show” körs nu vid klick.
-
+     let showing = document.querySelector(".clear");
+     showing.classList.toggle("clear_show");
+}
