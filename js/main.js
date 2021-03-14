f0cc4103bad66e5e4a4e8968f95eab0f61dbec0f
linn-holmqvist-nti-johanneberg <74715034+linn-holmqvist-nti-johanneberg@users.noreply.github.com>
2021-03-14 13:52:14 +0100

    03-14

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ function showmenu(){
      let showing = document.querySelector(".clear");
      showing.classList.toggle("clear_show");
 }
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides_imgs");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
